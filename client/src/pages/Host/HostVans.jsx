88a6078e065f2004d028647aa03d1e32f3a1668f
abdullmustyy <abdulmusty03@outlook.com>
2023-07-20 10:58:29 +0100

    Chore: Update VansPage and HostVansPage with usage instructions

--- client/src/pages/Host/HostVans.jsx
+++ client/src/pages/Host/HostVans.jsx
@@ -5,27 +5,32 @@ export default function HostVans() {
   const { hostVansData } = useSelector((state) => state.host);
 
   return (
-    <section className="bg-[#FFF7ED] py-8 md:px-0 px-4">
-      <div className="container mx-auto grid md:grid-cols-3 grid-cols-2 gap-8">
-        {hostVansData.map((vanData) => (
-          <Link to={`/host/vans/${vanData.id}`} key={vanData.id}>
-            <div className="flex flex-col space-y-2">
-              <h1 className="text-[#161616] text-lg font-semibold">
-                {vanData.name}
-              </h1>
-              <div className="rounded-tr-[3rem] rounded-bl-[3rem] outline outline-offset-2 outline-[1px]">
-                <img
-                  src={vanData.imageUrl}
-                  alt={vanData.name}
-                  className="rounded-tr-[3rem] rounded-bl-[3rem]"
-                />
+    <section className="bg-[#FFF7ED] pb-8 md:px-0 px-4">
+      <div className="container mx-auto">
+        <p className="text-lg font-light">
+          Click on a Van to see more details about it.
+        </p>
+        <div className="grid md:grid-cols-3 grid-cols-2 gap-8 pt-6">
+          {hostVansData.map((vanData) => (
+            <Link to={`/host/vans/${vanData.id}`} key={vanData.id}>
+              <div className="flex flex-col space-y-2">
+                <h1 className="text-[#161616] text-lg font-semibold">
+                  {vanData.name}
+                </h1>
+                <div className="rounded-tr-[3rem] rounded-bl-[3rem] outline outline-offset-2 outline-[1px]">
+                  <img
+                    src={vanData.imageUrl}
+                    alt={vanData.name}
+                    className="rounded-tr-[3rem] rounded-bl-[3rem]"
+                  />
+                </div>
+                <span className="text-[#161616] text-base font-medium self-end">
+                  ${vanData.price}/day
+                </span>
               </div>
-              <span className="text-[#161616] text-base font-medium self-end">
-                ${vanData.price}/day
-              </span>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          ))}
+        </div>
       </div>
     </section>
   );
--- client/src/pages/Vans/VansPage.jsx
+++ client/src/pages/Vans/VansPage.jsx
@@ -6,6 +6,7 @@ export default function VansPage() {
     <section className="container mx-auto my-12 md:px-0 px-4 text-[#161616]">
       <header className="mb-12 space-y-4">
         <h1 className="text-[2rem] font-semibold">Explore our van options</h1>
+        <p className="text-lg font-light">Pick a van you&apos;ll like to rent by clicking on it.</p>
         <VansFilters />
       </header>
       <main>

cc137e777eafacf032d6d4c26035462b133bd216
abdullmustyy <abdulmusty03@outlook.com>
2023-07-20 01:31:12 +0100

    Feat: Add Host Pricing and Host Van Photos page

--- client/src/pages/Host/HostPricing.jsx
+++ client/src/pages/Host/HostPricing.jsx
@@ -1,3 +1,11 @@
-export default function HostPricing() { 
-    return <h1>Pricing</h1>
+import { useSelector } from "react-redux";
+
+export default function HostPricing() {
+  const { hostVanDetails } = useSelector((state) => state.host);
+
+  return (
+    <section>
+      <span className="text-[#161616] text-2xl font-semibold">${hostVanDetails.price}/day</span>
+    </section>
+  );
 }
--- client/src/pages/Host/HostVanPhotos.jsx
+++ client/src/pages/Host/HostVanPhotos.jsx
@@ -1,3 +1,13 @@
+import { useSelector } from "react-redux";
+
 export default function HostVanPhotos() {
-  return <h1>HostVanPhotos</h1>;
+  const { hostVanDetails } = useSelector((state) => state.host);
+
+  return (
+    <section>
+      <div className="w-fit rounded-lg">
+        <img src={hostVanDetails.imageUrl} alt={hostVanDetails.name} className="w-40 object-cover rounded-lg" />
+      </div>
+    </section>
+  );
 }

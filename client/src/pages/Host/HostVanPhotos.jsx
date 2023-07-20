9df54bc7650481a39eb3a4cf82693270d2803776
abdullmustyy <abdulmusty03@outlook.com>
2023-07-20 01:33:33 +0100

    Chore: Format HostVanPhotos.jsx

--- client/src/pages/Host/HostVanPhotos.jsx
+++ client/src/pages/Host/HostVanPhotos.jsx
@@ -6,7 +6,11 @@ export default function HostVanPhotos() {
   return (
     <section>
       <div className="w-fit rounded-lg">
-        <img src={hostVanDetails.imageUrl} alt={hostVanDetails.name} className="w-40 object-cover rounded-lg" />
+        <img
+          src={hostVanDetails.imageUrl}
+          alt={hostVanDetails.name}
+          className="w-40 object-cover rounded-lg"
+        />
       </div>
     </section>
   );

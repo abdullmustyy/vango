3b15b2a994de43ceb9f8479b870edb5631c215ee
abdullmustyy <abdulmusty03@outlook.com>
2023-08-03 16:58:03 +0100

    Chore: Test responsiveness

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,9 @@
-export default function Footer() { 
-    return (
-      <footer className="bg-[#252525] text-[#AAAAAA] grid place-content-center py-6 text-base font-medium">
-        <div>
-          <span>Ⓒ 2022 #VANGO</span>
-        </div>
-      </footer>
-    );
+export default function Footer() {
+  return (
+    <footer className="bg-[#252525] text-[#AAAAAA] grid place-content-center py-6 mt-auto text-base font-medium">
+      <div>
+        <span>Ⓒ 2022 #VANGO</span>
+      </div>
+    </footer>
+  );
 }
